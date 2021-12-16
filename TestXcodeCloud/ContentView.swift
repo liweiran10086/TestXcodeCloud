ad101e293fc4c24c26fa055bc53dda8c642e272b
weirli <weirli@eclassifiedsgroup.com>
2021-12-16 15:48:24 +0800

    Initial Commit

---
+++ TestXcodeCloud/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestXcodeCloud
+//
+//  Created by Li, Dirk(AWF) on 2021/12/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestXcodeCloud/TestXcodeCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestXcodeCloudApp.swift
+//  TestXcodeCloud
+//
+//  Created by Li, Dirk(AWF) on 2021/12/16.
+//
+
+import SwiftUI
+
+@main
+struct TestXcodeCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
